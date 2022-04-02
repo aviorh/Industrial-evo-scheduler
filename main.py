1f454c2e2558178db2a3962cf32afe07896bfbd1
Avior Hay <wb-aviorhay552780@alibaba-inc.com>
2022-04-02 17:06:17 +0300

    defined data structures for the genetic engine

---
+++ main.py
@@ -0,0 +1,7 @@
+
+
+if __name__ == '__main__':
+    # set all values
+    # run algorithm
+    # output statistics
+    pass
--- src/genetic_engine/data_classes.py
+++ src/genetic_engine/data_classes.py
@@ -1,8 +1,38 @@
 from dataclasses import dataclass
+from typing import List, Dict
+
 
 @dataclass
 class Base:
     pass
 
+
 class ProductionLine(Base):
-    products
+    id: int
+    product_ids: Dict[int, int]  # bulk product id to capacity (kg/hour) per product. might need packaging capacity
+    manpower: int
+    setup_time: float
+    ffo: int
+
+
+@dataclass
+class BulkProduct(Base):
+    """Bissli Grill"""
+    id: int
+    recipe_id: int
+    cleaning_time: float
+    production_line_ids: List[int]  # production lines that can produce this bulk product
+
+
+@dataclass
+class Product(Base):
+    """Bissli Grill 100g"""
+    id: int
+    bulk_id: int  # fixme: might need to hold BulkProduct instance for mutation
+    name: str
+    priority: int  # 1 - 10 / low - urgent
+    weight: int  # grams
+    stock: int
+    forecast: int  # what needs to be produced over a week's course
+    unit_package_id: int
+    retailer_package_id: int
