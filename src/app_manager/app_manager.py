183a7ab92dd2091d9ef6f72f0f7e298ece4fef84
Avior Hay <aviorha@mta.ac.il>
2022-09-03 14:13:37 +0300

    finished working on edit solution in backend

--- src/app_manager/app_manager.py
+++ src/app_manager/app_manager.py
@@ -309,14 +309,14 @@ class AppManager(metaclass=SingletonMeta):
         found_event = False
         with db.auto_commit():
             new_solution = deepcopy(db_solution.solution)
-            raw_solution: np.ndarray = SolutionSchedule.convert_to_raw(new_solution['data'], site_data)
-            SolutionSchedule.verify_conversion(raw_solution, new_solution, site_data)
+            # raw_solution: np.ndarray = SolutionSchedule.convert_to_raw(new_solution['data'], site_data)
+            # SolutionSchedule.verify_conversion(raw_solution, new_solution, site_data)
 
-            db_solution.fitness = self.problems[db_solution.problem_id].engine._calculate_fitness(raw_solution)
+            # db_solution.fitness = self.problems[db_solution.problem_id].engine._calculate_fitness(raw_solution)
 
-            new_solution['forecast_achieved'] = SolutionAnalysis.get_achieved_forecast(raw_solution, site_data)
-            new_solution['raw_materials_usage'] = SolutionAnalysis.get_raw_materials_usage(raw_solution, site_data)
-            new_solution['product_line_utilization'] = SolutionAnalysis.get_product_lines_utilization(raw_solution, site_data)
+            # new_solution['forecast_achieved'] = SolutionAnalysis.get_achieved_forecast(raw_solution, site_data)
+            # new_solution['raw_materials_usage'] = SolutionAnalysis.get_raw_materials_usage(raw_solution, site_data)
+            # new_solution['product_line_utilization'] = SolutionAnalysis.get_product_lines_utilization(raw_solution, site_data)
 
             for event in new_solution['data'][str(production_line)]:
                 if event['key'] == key:
